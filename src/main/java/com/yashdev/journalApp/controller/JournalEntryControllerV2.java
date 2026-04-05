60cf3e098fb56962b6c4b3f5b27738a40326cf89
yashtiwari912 <yashtiwari92003@gmail.com>
2026-04-05 11:00:52 +0530

    Response Entity

--- src/main/java/com/yashdev/journalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/com/yashdev/journalApp/controller/JournalEntryControllerV2.java
@@ -5,15 +5,22 @@ import com.yashdev.journalApp.entity.JournalEntry;
 import com.yashdev.journalApp.services.JournalEntryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 //Standard Architecture for Spring Boot Application
 //Controller --> Service --> Repository --> Database
+
+//ResponseEntity<?> means that the response can be of any type, and it allows us to return different types of responses based on the situation.
+// For example, we can return a list of journal entries when the request is successful, or we can return an error message when something goes wrong.
+// This flexibility is useful for handling various scenarios in our API.
 
 @RestController
 @RequestMapping("/journal")
@@ -24,36 +31,55 @@ public class JournalEntryControllerV2 {
 
 
     @GetMapping
-    public List<JournalEntry> getAll(){
-        return journalEntryService.getAll();
+    public ResponseEntity<?> getAll(){
+        List<JournalEntry>all = journalEntryService.getAll();
+        if(all != null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public JournalEntry createEntry(@RequestBody JournalEntry myEntry){
-        myEntry.setDate(LocalDateTime.now());
-        journalEntryService.saveEntry(myEntry);
-        return myEntry;
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry){
+        try {
+            myEntry.setDate(LocalDateTime.now());
+            journalEntryService.saveEntry(myEntry);
+            return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     @GetMapping("id/{myId}")
-    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId){
-        return journalEntryService.getJournalEntryByID(myId).orElse(null);
+    public ResponseEntity<?> getJournalEntryById(@PathVariable ObjectId myId){
+        Optional<JournalEntry> journalEntry = journalEntryService.getJournalEntryByID(myId);
+        if(journalEntry.isPresent()){
+            return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
     @DeleteMapping("id/{myId}")
-    public boolean deleteJournalEntryById(@PathVariable ObjectId myId){
+    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId myId){
         journalEntryService.deleteById(myId);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
     @PutMapping("id/{id}")
-    public JournalEntry updateJournalEntryById(@PathVariable ObjectId id,@RequestBody JournalEntry newEntry){
+
+    public ResponseEntity<?> updateJournalEntryById(@PathVariable ObjectId id,@RequestBody JournalEntry newEntry){
         JournalEntry oldJournalEntry = journalEntryService.getJournalEntryByID(id).orElse(null);
         if(oldJournalEntry != null){
             oldJournalEntry.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : oldJournalEntry.getTitle() );
             oldJournalEntry.setContent(newEntry.getContent() != null && !newEntry.getContent().equals("") ? newEntry.getContent() : oldJournalEntry.getContent());
+            //I have updated oldJounalEntry with new Values and then saved them in db
+
+            journalEntryService.saveEntry(oldJournalEntry);
+            return new ResponseEntity<>(oldJournalEntry, HttpStatus.OK);
         }
-        //I have updated oldJounalEntry with new Values and then saved them in db
-        journalEntryService.saveEntry(oldJournalEntry);
-        return oldJournalEntry;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+
     }
 
 }
