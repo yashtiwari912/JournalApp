b077c1338ce7c480031cc0f2df37b1f122ce521c
yashtiwari912 <yashtiwari92003@gmail.com>
2026-05-24 16:30:51 +0530

    JavaMailSender

---
+++ src/test/java/com/yashdev/journalApp/services/EmailServiceTest.java
@@ -0,0 +1,16 @@
+package com.yashdev.journalApp.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTest {
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendEmail() {
+        emailService.sendEmail("test@example.com", "Test Subject", "Test Body");
+    }
+}
